65616234b6359b24200ed1b7da5f61e1452721af
Ismael <ismafacultad19@hotmail.com>
2020-05-28 18:13:11 -0300

    confirmo

--- Práctico 3/Ejercicio 5/interface.py
+++ Práctico 3/Ejercicio 5/interface.py
@@ -1,2 +1,11 @@
 from zope.interface import Interface
 
+class interface(Interface):
+    def insertarElemento(objeto, posicion):
+        pass
+    
+    def agregarElemento(objeto):
+        pass
+
+    def mostrarElemento(posicion):
+        pass
